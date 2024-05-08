52f141e254e5bc5886da4e823a940878115ea39f
vitaliy-pg <aljndrvt@myuax.com>
2024-05-08 10:12:57 +0200

    18

---
+++ src/main/java/Tweet.java
@@ -0,0 +1,11 @@
+public class Tweet {
+    private String content;
+
+    public Tweet(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
