41f84cbb4365c0184abf36e900d3bc230a08a74a
vitaliy-pg <aljndrvt@myuax.com>
2024-05-08 10:00:34 +0200

    2

--- src/main/java/UserAccount.java
+++ src/main/java/UserAccount.java
@@ -3,4 +3,9 @@ import java.util.List;
 
 
 public class UserAccount {
+    private String alias;
+    private Email email;
+    private List<Tweet> tweets;
+    private List<UserAccount> following;
+    private List<Tweet> timeline;
 }
