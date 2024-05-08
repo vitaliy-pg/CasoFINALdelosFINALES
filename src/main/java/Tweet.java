d8695435151c40eab55fa13f6191781a52eca10b
vitaliy-pg <aljndrvt@myuax.com>
2024-05-08 10:19:02 +0200

    20

--- src/main/java/Tweet.java
+++ src/main/java/Tweet.java
@@ -1,11 +1,34 @@
-public class Tweet {
-    private String content;
+import java.time.LocalDateTime;
 
-    public Tweet(String content) {
-        this.content = content;
+public class Tweet {
+    private LocalDateTime time;
+    private String message;
+    private UserAccount sender;
+
+    public Tweet(UserAccount sender, String message) {
+        this.sender = sender;
+        this.message = message;
+        this.time = LocalDateTime.now();
+
+        if (message.length() > 140) {
+            throw new IllegalArgumentException("Message cannot exceed 140 characters.");
+        }
     }
 
-    public String getContent() {
-        return content;
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public UserAccount getSender() {
+        return sender;
+    }
+
+    @Override
+    public String toString() {
+        return "Tweet from " + sender + " at " + time + ": \"" + message + "\"";
     }
 }
