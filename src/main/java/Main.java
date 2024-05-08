1d61235e3b09fa373a123e97c1e74fd937c70403
vitaliy-pg <aljndrvt@myuax.com>
2024-05-08 10:07:39 +0200

    11

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,4 +6,17 @@ public class Main {
     private static HashMap<String, UserAccount> accounts = new HashMap<>();
     private static Scanner scanner = new Scanner(System.in);
 
-}
+    public static void main(String[] args) {
+        boolean running = true;
+
+        while (running) {
+            System.out.println("\nTwitter Mini:");
+            System.out.println("1. Create User Account");
+            System.out.println("2. Follow User");
+            System.out.println("3. Post Tweet");
+            System.out.println("4. Show Account Info");
+            System.out.println("5. Exit");
+            System.out.print("Enter option: ");
+
+
+        }
