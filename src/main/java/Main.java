6fde2b42cde6b4afdbecb3054320a9f239222c2c
vitaliy-pg <aljndrvt@myuax.com>
2024-05-08 10:07:25 +0200

    10

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,8 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
+
+    private static HashMap<String, UserAccount> accounts = new HashMap<>();
+    private static Scanner scanner = new Scanner(System.in);
+
 }
