941848f7b51dbac926ce0cb474768006483da594
vitaliy-pg <aljndrvt@myuax.com>
2024-05-08 10:01:13 +0200

    3

--- src/main/java/UserAccount.java
+++ src/main/java/UserAccount.java
@@ -8,4 +8,19 @@ public class UserAccount {
     private List<Tweet> tweets;
     private List<UserAccount> following;
     private List<Tweet> timeline;
+
+
+public UserAccount(String alias, Email email) {
+    if (!Utils.isValidEmail(email.toString())) {
+        throw new IllegalArgumentException("Email inválido.");
+    }
+    if (!Utils.isValidAlias(alias)) {
+        throw new IllegalArgumentException("Alias inválido.");
+    }
+    this.alias = alias;
+    this.email = email;
+    this.tweets = new ArrayList<>();
+    this.following = new ArrayList<>();
+    this.timeline = new ArrayList<>();
 }
+
